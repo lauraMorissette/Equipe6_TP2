71bc824f3c5b33f787b9612c449e205d735281ba
lauraMorissette <79054874+lauraMorissette@users.noreply.github.com>
2023-05-06 16:19:36 -0400

    d

--- src/main.py
+++ src/main.py
@@ -20,6 +20,16 @@ distance =  [[0, 1.5, 1, 1.1, 2.3, 1.9, 2.7, 0.4],
 
 surface = [3000, 2000, 20000,800,4000,3600,25550,875]
 
+distance_depot = [[2.1],
+	    [1.75],
+	    [1.45],
+        [1.1],
+        [0.7],
+        [1],
+	    [0.125],
+	    [2.25]]
+CM_neige = 15
+
 solveurAMPL = solv_ampl.SolverAmpl()
-probleme = prob_park.ParkingProb(distance,)
-solveurAMPL.solve()
+probleme = prob_park.ParkingProb(distance,surface,CM_neige)
+solveurAMPL.solve(probleme)
