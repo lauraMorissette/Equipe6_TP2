2dcb6ffcbe9f425d1d33bf96345f797556d64c0a
lauraMorissette <79054874+lauraMorissette@users.noreply.github.com>
2023-05-10 18:53:18 -0400

    1234

--- src/parking.py
+++ src/parking.py
@@ -5,7 +5,7 @@ import sys
 class Parking(solu.Solution):
     def __init__(self, probleme_resolu=prob_park.ParkingProb()):
         super(Parking, self).__init__()
-
+        self.probleme = probleme_resolu
 
     def evaluate(self):
         pass
